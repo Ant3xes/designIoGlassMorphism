57ecd96bffd2352e2a803d1fad72d538890d18a4
Romain Pereira <46964039+Ant3xes@users.noreply.github.com>
2023-04-05 19:51:37 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+import Background from "./Components/Background";
+import Wave from "./Components/Wave";
+import TextSection from "./Components/TextSection";
+import Card from "./Components/Card";
+
+export default function App() {
+  return (
+    <Wrapper>
+      <Background />
+      <WaveWrapper>
+        <Wave />
+      </WaveWrapper>
+      <TextSection />
+      <CardWrapper>
+        <Card />
+      </CardWrapper>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const WaveWrapper = styled.div`
+  position: relative;
+  top: 200px;
+`;
+
+const CardWrapper = styled.div`
+  padding: 0 20px;
+`;
